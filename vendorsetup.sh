6d17752ba356f99568058b8aa173906e2e64654f
chematelegram <chfondia@gmail.com>
2023-12-30 15:13:52 +0100

    Remove the option to change splash image

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -72,6 +72,7 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
         export OF_DEFAULT_TIMEZONE="CET"
         export OF_FL_PATH1="/sys/class/leds/led:torch_0"
         export OF_FL_PATH2="/sys/class/leds/led:switch_0"
+        export OF_NO_SPLASH_CHANGE=1
 
 	# let's see what are our build VARs
 	if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
